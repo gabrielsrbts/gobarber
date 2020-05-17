d537953474ae5eb9273b9bdc4a76738e415415b1
Gabriel Resende <gabrielsrbts@gmail.com>
2020-05-17 10:19:07 -0300

    Commit inicial do projeto

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+// Importado somente o Router do express, ao invés do Express inteiro
+import { Router } from 'express';
+
+const routes = new Router();
+
+routes.get('/', (req, res) => {
+  return res.json({message: 'Hello Gabriel'});
+
+});
+
+
+
+export default routes;
