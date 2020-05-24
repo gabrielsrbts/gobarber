100f00bd4c76c475702d342e5382bbff8183be58
Gabriel Resende <gabrielsrbts@gmail.com>
2020-05-24 11:13:37 -0300

    Database configuration

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   dialect: 'postgres',
-  host: '172.28.134.76',
+  host: '172.26.216.76',
   username: 'postgres',
   password: 'docker',
   database: 'gobarber',
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 // Importado somente o Router do express, ao invés do Express inteiro
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Gabriel' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Gabriel Resende',
+    email: 'gabrielsrbts@gmail.com',
+    password_hash: '731278531283',
+  });
+  return res.json(user);
 });
 
 export default routes;
